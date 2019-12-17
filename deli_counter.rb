1f6da7c3dc9ef16aaccb16ed94497800e8ee0acc
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-12-17 22:33:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,4 @@
-# Write your code here
-
-
+def line(deli)
   if deli.empty?
     puts "The line is currently empty."
   else
@@ -10,7 +8,7 @@
     end
     puts current_line
   end
-
+end
 
 def take_a_number(deli, name)
   deli << name
